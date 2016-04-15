a9ca19cee186f0811a312609c0e6ec319bdf24a5
tensaix2j <tensaix2j@tensaix2j-VirtualBox.(none)>
2016-04-15 22:09:42 +0800

    modified:   error_controller.rb

--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -3,6 +3,12 @@ class ErrorController < ApplicationController
 	layout 'template'
 	
 	def not_found
+
+		@template_site = App.new()
+		[:title,:description,:favicon,:custom_css,:analytics,:footer].each {  |item|
+			@template_site[item] = ""
+		}
+		
 		detect_user_agent() 
 	end
 end
